b7d5cd167efec2a67df9e5b22433fdeb7494053b
felipesantoos <felipedssantos811@gmail.com>
2022-06-28 21:56:08 -0300

    Add text property in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-class Button extends React.Component {
+class Button extends React.Component<{text: string}> {
     render() {
         return (
-            <button className={style.button}>Add</button>
+            <button className={style.button}>{this.props.text}</button>
         );
     }
 }
